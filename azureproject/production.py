2b61284245d1dbad590bdceaca689d9ce5d29ce4
Saúl Yael Puente Ruiz <71954593+SaulPuente@users.noreply.github.com>
2023-05-14 00:31:45 -0600

    aa

--- azureproject/production.py
+++ azureproject/production.py
@@ -28,13 +28,14 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Configure Postgres database based on connection string of the libpq Keyword/Value form
 # https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING
 conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
+hostname = os.environ['DBHOST']
 conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': conn_str_params['dbname'],
-        'HOST': conn_str_params['host'],
-        'USER': conn_str_params['user'],
-        'PASSWORD': conn_str_params['password'],
+        'NAME': os.environ['DBNAME'],
+        'HOST': hostname + ".postgres.database.azure.com",
+        'USER': os.environ['DBUSER'],
+        'PASSWORD': os.environ['DBPASS'],
     }
 }
